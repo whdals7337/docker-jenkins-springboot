d82bd63155333d319b085f61baed2c66c507b416
Jong Min Yu <uok0201@gmail.com>
2021-01-31 19:12:00 +0900

    Initial commit

---
+++ src/test/java/com/sample/project/controller/HelloControllerTest.java
@@ -0,0 +1,41 @@
+package com.sample.project.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void before() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    @Test
+    public void hello() throws Exception {
+        // given
+        String url = "http://localhost:" + port;
+
+        // when
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+        // then
+    }
+}
